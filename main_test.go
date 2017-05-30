c3b2f34375ba953685c9641e6da4422c5ab3dbcb
Eric Fritz <eric@eric-fritz.com>
2017-05-30 18:01:36 -0500

    Update sweet entry point.

--- main_test.go
+++ main_test.go
@@ -4,13 +4,16 @@ import (
 	"testing"
 
 	"github.com/aphistic/sweet"
+	junit "github.com/aphistic/sweet-junit"
 	. "github.com/onsi/gomega"
 )
 
-func Test(t *testing.T) {
-	sweet.T(func(s *sweet.S) {
-		RegisterFailHandler(sweet.GomegaFail)
+func TestMain(m *testing.M) {
+	RegisterFailHandler(sweet.GomegaFail)
 
-		s.RunSuite(t, &ExecutorSuite{})
+	sweet.Run(m, func(s *sweet.S) {
+		s.RegisterPlugin(junit.NewPlugin())
+
+		s.AddSuite(&ExecutorSuite{})
 	})
 }
