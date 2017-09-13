de1506b8226c4006fd9240d5cbec252b8154ad12
Eric Fritz <eric@eric-fritz.com>
2017-09-12 19:22:06 -0500

    Update sweet.

--- main_test.go
+++ main_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/aphistic/sweet"
 	"github.com/aphistic/sweet-junit"
+	"github.com/efritz/backoff"
 	. "github.com/onsi/gomega"
 )
 
@@ -13,7 +14,81 @@ func TestMain(m *testing.M) {
 
 	sweet.Run(m, func(s *sweet.S) {
 		s.RegisterPlugin(junit.NewPlugin())
-
 		s.AddSuite(&ExecutorSuite{})
 	})
+}
+
+type ExecutorSuite struct{}
+
+var testBackoff = backoff.NewZeroBackoff()
+
+func (s *ExecutorSuite) TestSchedule(t sweet.T) {
+	var (
+		c = make(chan int)
+		r = make(chan int, 4)
+		e = NewExecutor(WithBackoff(testBackoff))
+	)
+
+	defer close(r)
+
+	e.Start()
+	e.Schedule(func() bool { <-c; r <- 1; return true })
+	e.Schedule(func() bool { r <- 2; return true })
+	e.Schedule(func() bool { r <- 3; return true })
+	e.Schedule(func() bool { r <- 4; return true })
+	close(c)
+
+	e.Flush()
+	Expect(r).Should(Receive(Equal(1)))
+	Expect(r).Should(Receive(Equal(2)))
+	Expect(r).Should(Receive(Equal(3)))
+	Expect(r).Should(Receive(Equal(4)))
+}
+
+func (s *ExecutorSuite) TestRetry(t sweet.T) {
+	var (
+		i = 0
+		j = 0
+		r = make(chan int)
+		e = NewExecutor(WithBackoff(testBackoff))
+	)
+
+	e.Start()
+	defer e.Stop()
+	defer close(r)
+
+	e.Schedule(func() bool { i++; r <- i; return i >= 20 })
+	e.Schedule(func() bool { j++; r <- j; return j >= 10 })
+
+	for k := 1; k <= 20; k++ {
+		Eventually(r).Should(Receive(Equal(k)))
+	}
+
+	for k := 1; k <= 10; k++ {
+		Eventually(r).Should(Receive(Equal(k)))
+	}
+
+	Eventually(r).ShouldNot(Receive())
+}
+
+func (s *ExecutorSuite) TestStop(t sweet.T) {
+	var (
+		r = make(chan int)
+		e = NewExecutor(WithBackoff(testBackoff))
+	)
+
+	defer close(r)
+
+	e.Start()
+	e.Schedule(func() bool { r <- 1; return true })
+	e.Schedule(func() bool { r <- 2; return true })
+	e.Schedule(func() bool { r <- 3; return true })
+	e.Schedule(func() bool { r <- 4; return true })
+	e.Schedule(func() bool { r <- 5; return true })
+
+	Eventually(r).Should(Receive(Equal(1)))
+	Eventually(r).Should(Receive(Equal(2)))
+	Eventually(r).Should(Receive(Equal(3)))
+	e.Stop()
+	Eventually(r).ShouldNot(Receive())
 }
