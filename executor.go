d8d33e02633bcc18236d69e560186729041e2c25
Eric Fritz <eric@eric-fritz.com>
2017-09-12 19:30:26 -0500

    Collapse files.

--- executor.go
+++ executor.go
@@ -185,3 +185,23 @@ func (e *executor) pop() (Task, bool) {
 	task, e.buffer = e.buffer[0], e.buffer[1:]
 	return task, true
 }
+
+func blockOnSignal(halt <-chan struct{}, ready <-chan struct{}) bool {
+	select {
+	case <-halt:
+		return false
+
+	case _, ok := <-ready:
+		return ok
+	}
+}
+
+func isClosed(ch <-chan struct{}) bool {
+	select {
+	case <-ch:
+		return true
+
+	default:
+		return false
+	}
+}
